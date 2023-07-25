fa91c45c231c91316ac73d6eaf06745ad24f79cd
fayyazAli24 <113125426+fayyazAli24@users.noreply.github.com>
2023-07-25 17:50:18 +1000

    mainScreen promotion done

--- lib/screens/home_screen/HomeScreen2.dart
+++ lib/screens/home_screen/HomeScreen2.dart
@@ -2,6 +2,7 @@ import 'package:ebotzz/services/mainScreenItems.dart';
 import 'package:ebotzz/utils/imports.dart';
 import 'package:ebotzz/widgets/customInput.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class HomeScreen2 extends StatelessWidget {
   const HomeScreen2({Key? key}) : super(key: key);
@@ -98,7 +99,7 @@ class HomeScreen2 extends StatelessWidget {
                           width: 200,
                           child: Padding(
                             padding: const EdgeInsets.all(8.0),
-                            child: Image(image: NetworkImage(MainScreenItems.recentlyViewdItems[0]["image"]!),fit: BoxFit.cover,),
+                            child: Image(image: NetworkImage("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjFDhQOorpTVrB-yDLq10ccJw1AEx1FhZgsQ&usqp=CAU"),fit: BoxFit.cover,),
                           ),
                         ),
                         Padding(
@@ -118,7 +119,59 @@ class HomeScreen2 extends StatelessWidget {
                     );
                   }),
                 ),
-              )
+              ),
+              SizedBox(height: 30,),
+              Container(
+                color: Colors.grey.shade300,
+                height: 170,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Hot Trades",style: TextStyle(fontSize: 24.sp),),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Row(
+                        children: [
+                          Expanded(child: Divider(thickness: 2,color: Colors.black,)),
+                          IconButton(onPressed: (){}, icon: Icon(Icons.handyman)),
+                          Expanded(child: Divider(thickness: 2,color: Colors.black,)),
+
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 20,),
+
+
+
+              Column(
+                children: List.generate(4, (index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 5),
+                    child: Container(
+                      width: 500,
+                      height: 200,
+                      decoration: BoxDecoration(
+                          color: index%2==0?Colors.orange:index%3==0?Colors.purple:Colors.red,
+                      ),
+                      child: Column(
+                        children: [
+                          Text("Electronics",style: TextStyle(fontSize: 24.sp,fontWeight: FontWeight.bold,color: Colors.white),),
+                          Text("Trade or Buy Electronics",style: TextStyle(color: Colors.white),),
+                          SizedBox(height: 20,),
+                          Container(
+                            width: 200,
+                            height: 100,
+                            child: Image(image: NetworkImage("https://static.vecteezy.com/system/resources/previews/014/033/452/original/wireless-headphones-front-view-white-icon-on-a-transparent-background-3d-rendering-png.png"))
+                            ,)
+                        ],
+                      ),
+                    ),
+                  );
+                }),
+              ),
 
 
 
